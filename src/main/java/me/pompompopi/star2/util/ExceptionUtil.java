6c975707368e57ea667b4fb7fe0f9647ff267387
pompompopi <143640209+pompompopi@users.noreply.github.com>
2023-12-07 10:05:25 +0000

    Remove unused code

--- src/main/java/me/pompompopi/star2/util/ExceptionUtil.java
+++ src/main/java/me/pompompopi/star2/util/ExceptionUtil.java
@@ -2,7 +2,6 @@ package me.pompompopi.star2.util;
 
 import me.pompompopi.star2.Star2;
 
-import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -43,24 +42,9 @@ public final class ExceptionUtil {
             handler.accept(t);
         });
     }
-    
+
     public static <T> CompletableFuture<T> handleExceptionAndLog(final CompletableFuture<T> completableFuture, final String where) {
         return handleException(completableFuture, t -> Star2.LOGGER.error("An exception occurred in {}", where, t));
-    }
-
-    @SafeVarargs
-    public static <T> T wrapMultiple(final UnstableSupplier<T> unstableSupplier, final Function<Exception, RuntimeException> exceptionMapper, final Class<? extends Exception>... exceptionClasses) {
-        if (!Arrays.stream(exceptionClasses).allMatch(Exception.class::isAssignableFrom))
-            throw new IllegalArgumentException("Provided non-exception class in accepted exceptions");
-        try {
-            return unstableSupplier.get();
-        } catch (Throwable ex) {
-            if (Arrays.stream(exceptionClasses).anyMatch(c -> c.isInstance(ex)))
-                throw exceptionMapper.apply((Exception) ex);
-            if (ex instanceof RuntimeException)
-                throw (RuntimeException) ex;
-            throw new RuntimeException(ex);
-        }
     }
 
     public static void ignore(final UnstableRunnable runnable) {
